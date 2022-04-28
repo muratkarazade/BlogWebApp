9215ca0ef0e45ebea4db3053858da860718c7a77
Murat Kara <muratkarazade@gmail.com>
2022-04-28 19:35:09 +0300

    Contact and about sections added .

---
+++ BlogWebApp/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = abm.GetList();
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+           
+            return PartialView();
+        }
+    }
+}
