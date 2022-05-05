e56431579073293edb3fb4ecb7c5287080e03f57
Murat Kara <muratkarazade@gmail.com>
2022-05-05 20:53:13 +0300

    Authorize Ve Startup.Cs Configure Service.

---
+++ BlogWebApp/Controllers/WriterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApp.Controllers
+{
+    public class WriterController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View(); 
+        }
+    }
+}
